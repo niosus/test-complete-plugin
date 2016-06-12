aa91b05c07d4d23a39d6899b41a171fa0dd07861
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2016-06-12 12:27:30 +0200

    minor updates

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 """ Usage: call with <filename>
 """
-from .clang import cindex38
-from .clang import utils
+import sys
 import platform
 from os import path
+sys.path.append(path.dirname(__file__))
+from clang import cindex38
+from clang import utils
 
 cindex = cindex38
 TU = cindex.TranslationUnit
@@ -12,14 +14,14 @@ filename = path.abspath("test.cpp")
 print(filename)
 clang_dir = ClangUtils.find_libclang_dir("clang++")
 if not cindex.Config.loaded:
-	cindex.Config.set_library_path(clang_dir)
+    cindex.Config.set_library_path(clang_dir)
 print("DIR!!! = ", clang_dir)
 print("platform: ", platform.architecture())
 print("python version: ", platform.python_version())
 clang_flags = ['-x', 'c++', '-std=c++11']
 tu = TU.from_source(filename=filename)
 tu.reparse()
-compl_obj = tu.codeComplete(path=filename, line=4, column=7)
+compl_obj = tu.codeComplete(path=filename, args=clang_flags, line=4, column=7)
 print(len(compl_obj.results))
 
 tu.reparse()
